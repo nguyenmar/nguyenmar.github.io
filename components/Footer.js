df6509be600d728354016da379244d3c8df4131d
Marian <mnguyen@computrolsystems.com>
2023-06-01 14:01:07 -0700

    changes

--- components/Footer.js
+++ components/Footer.js
@@ -34,15 +34,10 @@ export const Footer = () => {
 	return (
 		<footer className="bg-secondary text-center py-2 px-5">
 			<div className="container text-muted">
-				<small>&copy; 2021 {" "}
-					<Link href="https://github.com/hashirshoaeb">
-						<a>hashirshoaeb</a>
+				<small>&copy; 2023 {" "}
+					<Link href="https://github.com/nguyenmar">
+						<a>nguyenmar</a>
 					</Link>
-					. Open sourced with love under {" "}
-					<Link href="https://github.com/hashirshoaeb/portfolio/blob/main/LICENSE">
-						<a>MIT</a>
-					</Link>
-					{" "} License
 				</small>
 			</div>
 		</footer>
